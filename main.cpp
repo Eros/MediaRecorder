c452c6438130ac24396e8aed38d9d1612afea675
gp2-marr <george2.marr@live.uwe.ac.uk>
2018-05-04 21:04:10 +0100

    fucking hate sink info

--- main.cpp
+++ main.cpp
@@ -23,3 +23,15 @@ static void getServerInfoCallback(pa_context * c, const pa_server_info * i, void
     ((Main*)userdata) -> default_sink_name = i -> default_sink_name;
     m_api -> quit(m_api, 1);
 }
+
+static void getSinkInfoCallBack(pa_context *c, const pa_sink_info * i, int is_last, void *userdata){
+    if(is_last){
+        pa_operation_unref(pa_context_get_server_info(c, getServerInfoCallback, userdata));
+        return;
+    }
+    printf("Sink: %s - $s \n", i -> description, i -> name);
+}
+
+static void getSourceInfoCallBack(pa_context *c, const pa_source_info * i, int is_last, void *userdata){
+
+}
